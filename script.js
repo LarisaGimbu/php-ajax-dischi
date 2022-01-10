200d3fadb5611dc47589a7b42fd5f027c49ea83b
LarisaGimbu <gimbularisa@gmail.com>
2022-01-10 17:33:08 +0100

    select

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ const app = new Vue({
   el: '#app',
   data:{
     songs: [],
+    genres: [],
+    genreSelected: 'All',
+    apiUrl: 'http://localhost/php-ajax-dischi/server.php',
   },
   methods:{
     getApi(){
-      axios.get('http://localhost/php-ajax-dischi/server.php')
+      axios.get(this.apiUrl, {
+        params:{
+          genre: this.genreSelected
+        }
+      })
         .then( response =>{
           this.songs = response.data;
+
+          this.songs.forEach(song => {
+            if(!this.genres.includes(song.genre)){
+              this.genres.push(song.genre);
+              
+            }
+          });
+          console.log(this.genres);
           console.log(this.songs);
 
         })
--- server.php
+++ server.php
@@ -2,7 +2,17 @@
 
 require_once __DIR__ . '/database.php';
 
-$discs = $songs;
+$discs = [];
+
+if($_GET['genre'] === 'All'){
+  $discs = $songs;
+}else{
+  foreach($songs as $song){
+    if($_GET['genre'] === $song['genre']){
+      $discs[] = $song;
+    }
+  }
+}
 
 header("Content-Type: application/json;charset=utf-8");
 
